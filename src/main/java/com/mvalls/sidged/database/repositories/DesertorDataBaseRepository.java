ab0ea2bebd1a4e3c2e90684581644456aead6677
fernando <fernandodanielvalls@gmail.com>
2021-08-01 22:22:10 -0300

    llegue hasta aca, mañana sigo chusmenado lo que hice paso por paso para entender mejor, ahora voy a googlear LOMBOK

---
+++ src/main/java/com/mvalls/sidged/database/repositories/DesertorDataBaseRepository.java
@@ -0,0 +1,23 @@
+package com.mvalls.sidged.database.repositories;
+
+
+import com.mvalls.sidged.core.model.Desertor;
+import com.mvalls.sidged.core.repositories.DesertorRepository;
+import com.mvalls.sidged.database.mybatis.mappers.DesertorMapper;
+import com.mvalls.sidged.database.repositories.mappers.DesertorRepositoryDTOMapper;
+
+public class DesertorDataBaseRepository implements DesertorRepository{
+
+    private final DesertorMapper desertorMapper;
+	private final DesertorRepositoryDTOMapper dtoMapper = new DesertorRepositoryDTOMapper();
+
+    public DesertorDataBaseRepository(DesertorMapper desertorMapper) {
+		this.desertorMapper = desertorMapper;
+	}
+
+    @Override
+    public Desertor findByLanguage(String word) {
+		return dtoMapper.dtoToModel(this.desertorMapper.findByLanguage(word));
+	}
+    
+}
---
+++ src/main/resources/setup/DESERTOR_BATCH_CONFIG.sql
@@ -0,0 +1,6 @@
+CREATE TABLE DESERTOR_BATCH_CONFIG (
+    ID bigint,
+    TEXT varchar(2000) NOT null,
+    LANGUAGE VARCHAR(2),
+    primary key (ID)
+);
